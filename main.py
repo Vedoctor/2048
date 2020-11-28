38df0fb95442cf3219cbd8597f4ac0224c86dc5d
vedoctor <mico.mg52@gmail.com>
2020-11-28 08:35:18 +0100

    chyba dodalem obrazki do gita ale nie wiem

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ HEIGHT = 500
 
 blocks = []
 
-pos_list = [50, 150, 250, 350]
 
 def draw():
     screen.fill('black')
     for block in blocks:
-        screen.draw.filled_rect(block, 'white')
+        if block.value == 2:
+            block.draw()
+        if block.value == 4:
+            block.draw()
+            block.image = '2048_4'
+        if block.value == 8:
+            block.draw()
+            block.image = '2048_8'
+        if block.value == 16:
+            block.draw()
+            block.image = '2048_16'
+        if block.value == 32:
+            block.draw()
+            block.image = '2048_32'
+        if block.value == 64:
+            block.draw()
+            block.image = '2048_64'
+
 
 
 def on_key_down(key):
@@ -30,47 +46,57 @@ def on_key_down(key):
         move_down()
 
 def add_block():
-    block = Rect(
-        random.choice(pos_list),
-        random.choice(pos_list),
-        100, 100)
+    block = Actor('2048_2')
+    block.posx = random.randint(1, 4)
+    block.posy = random.randint(1, 4)
+    rnd = random.randint(0, 100)
+    if rnd < 20:
+        block.value = 4
+    else:
+        block.value = 2
     blocks.append(block)
 
 
 def update():
-    pass
+    for block in blocks:
+        block_pos(block, block.posx, block.posy)
+
+
+def block_pos(block, posx, posy):
+    block.x = posx * 100
+    block.y = posy * 100
 
 
 def move_left():
     for block in blocks:
-        if block.x != 50:
-            block.x -= 100
+        if block.posx != 1:
+            block.posx -= 1
     add_block()
 
 
 def move_right():
     for block in blocks:
-        if block.x != 350:
-            block.x += 100
+        if block.posx != 4:
+            block.posx += 1
     add_block()
 
 
 def move_up():
     for block in blocks:
-        if block.y != 50:
-            block.y -= 100
+        if block.posy != 1:
+            block.posy -= 1
     add_block()
 
 
 def move_down():
     for block in blocks:
-        if block.y != 350:
-            block.y += 100
+        if block.posy != 4:
+            block.posy += 1
     add_block()
 
 
+
 add_block()
 add_block()
-print(blocks)
 
 pg.go()
