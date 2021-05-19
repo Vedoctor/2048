3ee31bf66b0aa45512732ae2e502f898eaa86041
vedoctor <mico.mg52@gmail.com>
2021-05-19 21:47:00 +0200

    nie wiem

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ WIDTH = 500
 HEIGHT = 500
 
 blocks = []
-size = 4
+positions = []
 
 def draw():
     screen.fill('black')
@@ -28,7 +28,6 @@ def draw():
             block.image = '2048_64'
 
 
-
 def on_key_down(key):
     if key == keys.ESCAPE:
         sys.exit()
@@ -45,96 +44,45 @@ def on_key_down(key):
     if key == keys.DOWN:
         move_down()
 
+
 def add_block():
     block = Actor('2048_2')
-    posx = 0
-    while posx == 0:
-        posx, posy = get_available_spot()
-    block.posx = posx
-    block.posy = posy
-    rnd = random.randint(0, 100)
-    if rnd < 20:
+    block.position = [random.randint(1, 4), 2]
+    if len(blocks) != 0:
+        for other_block in blocks:
+            if other_block.position == block.position:
+                add_block()
+    rnd = random.randint(1, 100)
+    if rnd <= 20:
         block.value = 4
     else:
         block.value = 2
     blocks.append(block)
-
-
-def get_available_spot():
-    posx = random.randint(1, 4)
-    posy = random.randint(1, 4)
-    for block in blocks:
-        if block.posx == posx and block.posy == posy:
-            return 0, 0
-    return posx, posy
 
 
 def update():
     for block in blocks:
-        block.x = block.posx * 100
-        block.y = block.posy * 100
+        block.pos = block.position[0] * 100, block.position[1] * 100
+    update_positions()
 
-    if len(blocks) == pow(size, 2):
-        sys.exit()
 
+def update_positions():
+    for block in blocks:
+        positions.clear()
+        positions.append(block.position)
 
 
 def move_left():
-    for i in range(size):
+    for i in range(4):
         for block in blocks:
-            action = can_move(block, 'left')
-            if action == 'move' and block.posx != 1:
-                block.posx -= 1
-            elif action == 'merge':
-                block.value += block.value
-                block.posx -= 1
-    add_block()
+            for position in positions:
+                if block.position[0] - 1 == position[0]:
+                    continue
+            if block.position[0] != 1:
+                block.position[0] -= 1
 
 
-def move_right():
-    for block in blocks:
-        if block.posx != 4:
-            block.posx += 1
-    add_block()
+add_block()
+add_block()
 
-
-def move_up():
-    for block in blocks:
-        if block.posy != 1:
-            block.posy -= 1
-    add_block()
-
-
-def move_down():
-    for block in blocks:
-        if block.posy != 4:
-            block.posy += 1
-    add_block()
-
-
-def can_move(block, dir):
-    posx = block.posx
-    posy = block.posy
-    for block2 in blocks:
-        if block2.posx == posx - 1 and block2.posy == posy:
-            if block2.value == block.value:
-                blocks.remove(block2)
-                return 'merge'
-            return 'nomerge'
-    return 'move'
-
-
-def add_first_blocks():
-    block = Actor('2048_2')
-    block.posx = random.randint(1, 4)
-    block.posy = random.randint(1, 4)
-    rnd = random.randint(0, 100)
-    if rnd < 20:
-        block.value = 4
-    else:
-        block.value = 2
-    blocks.append(block)
-
-
-add_first_blocks()
 pg.go()
